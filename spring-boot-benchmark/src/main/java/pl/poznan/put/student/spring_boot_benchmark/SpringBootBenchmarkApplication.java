74fff4093c45bf43e47496a5d8d5ee4af77bb15d
dawidnowakowski <89041701+dawidnowakowski@users.noreply.github.com>
2026-05-15 17:17:23 +0200

    Add base projects

---
+++ spring-boot-benchmark/src/main/java/pl/poznan/put/student/spring_boot_benchmark/SpringBootBenchmarkApplication.java
@@ -0,0 +1,13 @@
+package pl.poznan.put.student.spring_boot_benchmark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBenchmarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBenchmarkApplication.class, args);
+	}
+
+}
---
+++ spring-boot-benchmark/src/test/java/pl/poznan/put/student/spring_boot_benchmark/SpringBootBenchmarkApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.poznan.put.student.spring_boot_benchmark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBenchmarkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
